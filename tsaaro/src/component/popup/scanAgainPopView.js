002a41038b105afa9592f85a5d5299fbf4235909
Vivek <viveksingh2893@gmail.com>
2022-03-13 03:12:28 +0530

    all popups

--- tsaaro/src/component/popup/scanAgainPopView.js
+++ tsaaro/src/component/popup/scanAgainPopView.js
@@ -5,19 +5,12 @@ import "../../assets/css/scanagainpop.css";
 function ScanAgainPop({ closeScan }) {
   return (
     <div className="scan">
-      <div
-        onClick={() => {
-          closeScan(false);
-        }}
-        className="scan-overlay"
-      ></div>
+      <div onClick={() => {closeScan(false)}} className="scan-overlay"></div>
       <div className="scan-content">
         <div className="scan-img">
-            <img src={img} className="scan-tick"/>
+            <img src={img}/>
         </div>
-        <div className="scan-heading">
-          <h3>Scanning initiated successfully</h3>
-        </div>
+          <h3 className="scan-heading">Scanning initiated successfully</h3>
         <div className="scan-text">
           It might take a few minutes to a few hours to complete the scanning of
           your website. This depends on the number of pages to scan and the
@@ -25,9 +18,7 @@ function ScanAgainPop({ closeScan }) {
           cookie list automatically once the scanning is completed.
         </div>
         <div className="scan-btn">
-            <button onClick={() => {
-          closeScan(false);
-        }} className="scan-ok">OK</button>
+            <button onClick={() => {closeScan(false)}} className="scan-ok">OK</button>
         </div>
       </div>
     </div>
