982c9db17b92af2a0a96105478f7d5bb5f922681
shashwatpandey77 <your_github_email>
2022-03-14 14:50:14 +0530

    Consent Banner Initial

--- tsaaro/src/component/header/SideMenuView.js
+++ tsaaro/src/component/header/SideMenuView.js
@@ -3,10 +3,10 @@ import { Col, Image, Row } from "antd";
 import "../../assets/css/sidemenu.css";
 import logo from "../../assets/images/img1.png";
 import SmButton from "./smButton";
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function SideMenuView(props) {
-  return ( 
+  return (
     <div className="box">
       <div className="img">
         <div className="img1">
@@ -27,25 +27,30 @@ function SideMenuView(props) {
 
       <div className="btn">
         <NavLink to=".\dash">
-          <SmButton id="dsboard" name="Dashboard" inactive={props.inactive}/>
+          <SmButton id="dsboard" name="Dashboard" inactive={props.inactive} />
         </NavLink>
-        <NavLink to=".\cb">
-          <SmButton id="cb" name="Consent Banner" inactive={props.inactive}/>
-        </NavLink>
+        <div
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            props.callcb();
+          }}
+        >
+          <SmButton id="cb" name="Consent Banner" inactive={props.inactive} />
+        </div>
         <NavLink to=".\cm">
-          <SmButton id="cm" name="Cookie Manager" inactive={props.inactive}/>
+          <SmButton id="cm" name="Cookie Manager" inactive={props.inactive} />
         </NavLink>
         <NavLink to=".\cl">
-          <SmButton id="cl" name="Consent Log" inactive={props.inactive}/>
+          <SmButton id="cl" name="Consent Log" inactive={props.inactive} />
         </NavLink>
         <NavLink to=".\pp">
-          <SmButton id="pp" name="Privacy Policy" inactive={props.inactive}/>
+          <SmButton id="pp" name="Privacy Policy" inactive={props.inactive} />
         </NavLink>
         <NavLink to=".\cp">
-          <SmButton id="cp" name="Cookie Policy" inactive={props.inactive}/>
+          <SmButton id="cp" name="Cookie Policy" inactive={props.inactive} />
         </NavLink>
         <NavLink to=".\ss">
-          <SmButton id="ss" name="Site Settings" inactive={props.inactive}/>
+          <SmButton id="ss" name="Site Settings" inactive={props.inactive} />
         </NavLink>
       </div>
     </div>
--- tsaaro/src/screen/dashboard/index.js
+++ tsaaro/src/screen/dashboard/index.js
@@ -1,60 +1,70 @@
-import React, {useState, useEffect} from 'react';
-import HeaderMenuView from '../../component/header/HeaderMenuView';
-import SideMenuView from '../../component/header/SideMenuView';
-import {
-    BrowserRouter,
-    Routes,
-    Route
-  } from "react-router-dom";
-import CookiemanagerScr from './cookiemanager';
-import ConsentbannerScr from './consentbanner';
-import CookiepolicyScr from './cookiepolicy';
-import PrivacypolicyScr from './privacypolicy';
-import ConsentlogScr from './consentlog';
-import DashboardScr from './dashboard';
-import SitesettingScr from './sitesetting';
-import '../../assets/css/sidemenu.css'
- 
-const Screen=()=>{
-    const [inactive, setInactive] = useState(false);
-    // const [viewPortWidth, setWidth] = useState(0);
-    // const [viewPortHeight, setHeight] = useState(0)
+import React, { useState, useEffect } from "react";
+import HeaderMenuView from "../../component/header/HeaderMenuView";
+import SideMenuView from "../../component/header/SideMenuView";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CookiemanagerScr from "./cookiemanager";
+import ConsentbannerScr from "./consentbanner";
+import CookiepolicyScr from "./cookiepolicy";
+import PrivacypolicyScr from "./privacypolicy";
+import ConsentlogScr from "./consentlog";
+import DashboardScr from "./dashboard";
+import SitesettingScr from "./sitesetting";
+import "../../assets/css/sidemenu.css";
+import SvgIcon from "../../assets/Icon_apps";
 
-    // useEffect(() => {
-    //     window.scrollTo(0, 0);
-    //   }, []);
-    
-    //   useEffect(() => {
-    //     setWidth(window.innerWidth);
-    //     window.addEventListener("resize", (e) => {
-    //       console.log("size", e.target);
-    //       setWidth(window.innerWidth);
-    //       setHeight(window.innerHeight);
-    //     });
-    //   }, []);
+const Screen = () => {
+  const [inactive, setInactive] = useState(false);
+  const [cbvisible, setCbvisible] = useState(false);
+  // const [viewPortWidth, setWidth] = useState(0);
+  // const [viewPortHeight, setHeight] = useState(0)
 
-    return(
-        <div style={{display:'flex', flexDirection:'row'}}>
-            <div className={`sidenav ${inactive ? "inactive" : ""}`} >
-                <SideMenuView inactive={inactive}/>
-            </div>
-            <div style={{display:'flex', flexDirection:'column'}}>
-                <HeaderMenuView inactive={inactive} setInactive={setInactive}/>
-                <div style={{display:'flex',backgroundColor:'#E5E5E5'}}>
-                <div style={{display:'flex', width:inactive?'89vw':'82vw'}}>
-                <Routes>
-                    <Route path="/dash" element={<DashboardScr/>}/>
-                    <Route path="/cm" element={<CookiemanagerScr/>}/>
-                    <Route path="/cb" element={<ConsentbannerScr/>}/>
-                    <Route path="/cl" element={<ConsentlogScr/>}/>
-                    <Route path="/cp" element={<CookiepolicyScr/>}/>
-                    <Route path="/pp" element={<PrivacypolicyScr/>}/>
-                    <Route path="/ss" element={<SitesettingScr/>}/>
-                </Routes>
-                </div>
-                </div>
-            </div>
+  // useEffect(() => {
+  //     window.scrollTo(0, 0);
+  //   }, []);
+
+  //   useEffect(() => {
+  //     setWidth(window.innerWidth);
+  //     window.addEventListener("resize", (e) => {
+  //       console.log("size", e.target);
+  //       setWidth(window.innerWidth);
+  //       setHeight(window.innerHeight);
+  //     });
+  //   }, []);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      {cbvisible ? (
+        <ConsentbannerScr
+          callcb={() => {
+            setCbvisible(!cbvisible);
+          }}
+        />
+      ) : null}
+      <div className={`sidenav ${inactive ? "inactive" : ""}`}>
+        <SideMenuView
+          inactive={inactive}
+          callcb={() => {
+            setCbvisible(!cbvisible);
+          }}
+        />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <HeaderMenuView inactive={inactive} setInactive={setInactive} />
+        <div style={{ display: "flex", backgroundColor: "#E5E5E5" }}>
+          <div style={{ display: "flex", width: inactive ? "89vw" : "82vw" }}>
+            <Routes>
+              <Route path="/dash" element={<DashboardScr />} />
+              <Route path="/cm" element={<CookiemanagerScr />} />
+              <Route path="/cb" element={<ConsentbannerScr />} />
+              <Route path="/cl" element={<ConsentlogScr />} />
+              <Route path="/cp" element={<CookiepolicyScr />} />
+              <Route path="/pp" element={<PrivacypolicyScr />} />
+              <Route path="/ss" element={<SitesettingScr />} />
+            </Routes>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 export default Screen;
