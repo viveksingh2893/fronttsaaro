3a56da20205e0366919f789d4c573d7ea1fc4e33
Vivek <viveksingh2893@gmail.com>
2022-03-17 02:27:57 +0530

    50% consent log

---
+++ tsaaro/src/component/content/consentLogView.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../assets/css/consentlog.css";
+import SvgIcon from "../../assets/Icon_apps";
+
+const ConsentLogView=(props)=>{
+    return(
+        <div className="consentlog-block">
+            <p className="consent-ip">{props.ip}</p>
+            <p className="consent-ip">{props.country}</p>
+            <div className="consent-ip" style={{display:'flex', flexDirection:'row',justifyContent:'space-between'}}>
+                <p style={{color:props.accept?'green':'red',margin:'0vw'}}>{props.status}</p>
+                <div className="consentlog-eye">
+                    <SvgIcon name='eye'/>
+                </div>
+            </div>
+            <p className="consent-ip">{props.time}</p>
+        </div>
+    )
+}
+
+export default ConsentLogView;
--- tsaaro/src/screen/dashboard/consentlog.js
+++ tsaaro/src/screen/dashboard/consentlog.js
@@ -1,11 +1,82 @@
-const ConsentlogScr=()=>{
-    return(
-        <div  style={{display:'flex',height:'100vh',height:'40vw',justifyContent:'center',alignItems:'center'}}>
+import React, {useState} from "react";
+import { Col, Button, Row } from "antd";
+import "../../assets/css/consentlog.css";
+import ConsentLogView from "../../component/content/consentLogView";
+import ConsentPopView from "../../component/popup/consentPopView";
 
+const ConsentlogScr = (props) => {
+  const [scan, setScan] = useState(false);
+  const [accept, setAccept] = useState(true)
 
-        <h1>Hello Consent Log</h1>
+  const toggleScan = () => {
+    setScan(!scan);
+  };
+  
+  return (
+    <div className="consentlog-maincontainer">
+      <Row
+        style={{
+          display: "flex",
+          marginTop:'5vh',
+          alignItems: "center",
+          justifyContent: "flex-start",
+        }}
+      >
+        <Col span={12}>
+          <h2 className="consentlog-heading">Visitor Consent Log</h2>
+        </Col>
+        <Col
+          span={12}
+          style={{
+            display: "flex",
+            alignItems: "center", 
+            justifyContent: "flex-end",
+          }}
+        >
+            <Button onClick={toggleScan} type="primary" className="consentlog-scanbtn">
+              <p className="consentlog-scanagain">Scan Again</p>
+            </Button>
+        </Col>
+      </Row>
+      <Row>
+        <p className="consentlog-description">
+        Details of all the consent provided by the visitors. 
+        You may use this record as a proof of consent in case of an audit.
+        </p>
+      </Row>
+      <div style={{display:'flex',flexDirection:'column',padding:'2vw', backgroundColor:'white', borderRadius:'4px',boxShadow:'0px 0.5px 4px rgba(0, 0, 0, 0.2)'}}>
+        <div style={{display:'flex', flexDirection:'row', alignItems:'center',justifyContent:'flex-start'}}>
+            <p className="consentlog-title">IP Address</p>
+            <p className="consentlog-title">Country</p>
+            <p className="consentlog-title">Consent Status</p>
+            <p className="consentlog-title">Date/Time (UTC ± 00.00)</p>
         </div>
-    );
-}
+        <ConsentLogView  
+            accept={accept}
+            ip='49.26.451.0'
+            country='India'
+            status={accept?'accepted':'rejected'}
+            time='14-02-2022, 08:02:22'
+        />
+        <ConsentLogView  
+            accept={false}
+            ip='49.26.451.0'
+            country='India'
+            status={accept?'accepted':'rejected'}
+            time='14-02-2022, 08:02:22'
+        />
+      </div>
+      <p className="consentlog-btmline">You can view up to 100 visitor consents logged here. Use the export option if you want to download the historical log data.</p>
+      {scan && (
+        <div className="consentlog-modal">
+            <div className="consentlog-modalcontent">
+                <ConsentPopView closeScan={setScan}/>
+            </div>
+        </div> 
+      )}
+    </div>
+  );
+};
+
 
 export default ConsentlogScr;
