4c5a6cdf736751e56137e001cc670bc70437b232
Vivek <viveksingh2893@gmail.com>
2022-03-24 18:02:18 +0530

    selected button is visible

--- tsaaro/src/component/header/SideMenuView.js
+++ tsaaro/src/component/header/SideMenuView.js
@@ -5,7 +5,80 @@ import logo from "../../assets/images/img1.png";
 import SmButton from "./smButton";
 import { NavLink } from "react-router-dom";
 
-function SideMenuView(props) {
+const SideMenuView=(props)=> {
+  const [color, setColor]= useState(false);
+
+  const [color1, setColor1] = useState(false); 
+  const [color2, setColor2] = useState(false); 
+  const [color3, setColor3] = useState(false); 
+  const [color4, setColor4] = useState(false); 
+  const [color5, setColor5] = useState(false); 
+  const [color6, setColor6] = useState(false); 
+  const [color7, setColor7] = useState(false);  
+
+  const toggle1=()=>{
+      setColor1(true);
+      setColor2(false);
+      setColor3(false);
+      setColor4(false);
+      setColor5(false);
+      setColor6(false);
+      setColor7(false);
+  }
+  const toggle2=()=>{
+      setColor1(false);
+      setColor2(true);
+      setColor3(false);
+      setColor4(false);
+      setColor5(false);
+      setColor6(false);
+      setColor7(false);
+    }
+  const toggle3=()=>{
+      setColor1(false);
+      setColor2(false);
+      setColor3(true);
+      setColor4(false);
+      setColor5(false);
+      setColor6(false);
+      setColor7(false);
+    }
+  const toggle4=()=>{
+      setColor1(false);
+      setColor2(false);
+      setColor3(false);
+      setColor4(true);
+      setColor5(false);
+      setColor6(false);
+      setColor7(false);
+    }
+  const toggle5=()=>{
+      setColor1(false);
+      setColor2(false);
+      setColor3(false);
+      setColor4(false);
+      setColor5(true);
+      setColor6(false);
+      setColor7(false);
+    }
+  const toggle6=()=>{
+      setColor1(false);
+      setColor2(false);
+      setColor3(false);
+      setColor4(false);
+      setColor5(false);
+      setColor6(true);
+      setColor7(false);
+    }
+  const toggle7=()=>{
+      setColor1(false);
+      setColor2(false);
+      setColor3(false);
+      setColor4(false);
+      setColor5(false);
+      setColor6(false);
+      setColor7(true);
+  }
 
   return (
 
@@ -28,31 +101,32 @@ function SideMenuView(props) {
       ></div>
 
       <div className="btn">
-        <NavLink to=".\dash">
-          <SmButton id="dsboard" name="Dashboard" inactive={props.inactive}/>
+        <NavLink to=".\dash" onClick={toggle1}>
+          <SmButton id="dsboard" name="Dashboard" inactive={props.inactive} color={color1}/>
         </NavLink>
         <div
           style={{ cursor: "pointer" }}
           onClick={() => {
             props.callcb();
+            toggle2();
           }}
         >
-          <SmButton id="cb" name="Consent Banner" inactive={props.inactive} />
+          <SmButton id="cb" name="Consent Banner" inactive={props.inactive} color={color2} />
         </div>
-        <NavLink to=".\cm">
-          <SmButton id="cm" name="Cookie Manager" inactive={props.inactive} />
+        <NavLink to=".\cm" onClick={toggle3}>
+          <SmButton id="cm" name="Cookie Manager" inactive={props.inactive} color={color3}/>
         </NavLink>
-        <NavLink to=".\cl">
-          <SmButton id="cl" name="Consent Log" inactive={props.inactive} />
+        <NavLink to=".\cl" onClick={toggle4}>
+          <SmButton id="cl" name="Consent Log" inactive={props.inactive} color={color4}/>
         </NavLink>
-        <NavLink to=".\pp">
-          <SmButton id="pp" name="Privacy Policy" inactive={props.inactive} />
+        <NavLink to=".\pp" onClick={toggle5}>
+          <SmButton id="pp" name="Privacy Policy" inactive={props.inactive} color={color5}/>
         </NavLink>
-        <NavLink to=".\cp">
-          <SmButton id="cp" name="Cookie Policy" inactive={props.inactive} />
+        <NavLink to=".\cp" onClick={toggle6}>
+          <SmButton id="cp" name="Cookie Policy" inactive={props.inactive} color={color6} />
         </NavLink>
-        <NavLink to=".\ss">
-          <SmButton id="ss" name="Site Settings" inactive={props.inactive} />
+        <NavLink to=".\ss" onClick={toggle7}>
+          <SmButton id="ss" name="Site Settings" inactive={props.inactive} color={color7}/>
         </NavLink>
       </div>
     </div>
--- tsaaro/src/component/header/smButton.js
+++ tsaaro/src/component/header/smButton.js
@@ -1,35 +1,12 @@
 import React, {useState, useEffect} from "react";
 import SvgIcon from "../../assets/Icon_apps";
 import '../../assets/css/sidemenu.css';
+import ColumnGroup from "antd/lib/table/ColumnGroup";
  
 const SmButton= (props)=>{
-//   const [color, setColor] = useState(false); 
-
-//   const toggle=()=>{
-//     if(props.id === 'dsboard'){
-//       props.id==='dsboard'?setColor(true):setColor(false);
-//     }else if(props.id === 'cb'){
-//       props.id==='cb'?setColor(true):setColor(false);
-//     }else if(props.id === 'cm'){
-//       props.id==='cm'?setColor(true):setColor(false);
-//     }else if(props.id === 'cl'){
-//       props.id==='cl'?setColor(true):setColor(false);
-//     }else if(props.id === 'pp'){
-//       props.id==='pp'?setColor(true):setColor(false);
-//     }else if(props.id === 'cp'){
-//       props.id==='cp'?setColor(true):setColor(false);
-//     }else if(props.id === 'cp'){
-//       props.id==='?setColor(true):setColor(false);
-//     }else if(props.id === 'ss'){
-//       props.id?setColor(true):setColor(false);
-//   }
-// }
-
-//   console.log('color', color)
-
   
 return(
-    <div className="button">
+    <div className="button" style={{backgroundColor:props.color?'#7A4EB6':''}}>
       <div className={`${props.inactive ? "button-child2" : "button-child"}`}>
         <div style={{display:'flex', alignItems:'flex-start',width:'3vw', justifyContent:'center'}}><SvgIcon name={props.name.toLowerCase()}/></div>
         {props.inactive?null:<p className="button-para" style={{display:'flex', width:'8vw',justifyContent:'flex-start'}}>{props.name}</p>}
