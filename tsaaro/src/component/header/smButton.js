5b01894c98af1cab1536f29f7d4434304f8fcaf2
Vivek <viveksingh2893@gmail.com>
2022-03-24 16:55:57 +0530

    bugs removed

--- tsaaro/src/component/header/smButton.js
+++ tsaaro/src/component/header/smButton.js
@@ -3,11 +3,30 @@ import SvgIcon from "../../assets/Icon_apps";
 import '../../assets/css/sidemenu.css';
  
 const SmButton= (props)=>{
-  // const [color, setColor] = useState(false); 
+//   const [color, setColor] = useState(false); 
 
-  // const toggle=()=>{
-  //   setColor(!color)
-  // }
+//   const toggle=()=>{
+//     if(props.id === 'dsboard'){
+//       props.id==='dsboard'?setColor(true):setColor(false);
+//     }else if(props.id === 'cb'){
+//       props.id==='cb'?setColor(true):setColor(false);
+//     }else if(props.id === 'cm'){
+//       props.id==='cm'?setColor(true):setColor(false);
+//     }else if(props.id === 'cl'){
+//       props.id==='cl'?setColor(true):setColor(false);
+//     }else if(props.id === 'pp'){
+//       props.id==='pp'?setColor(true):setColor(false);
+//     }else if(props.id === 'cp'){
+//       props.id==='cp'?setColor(true):setColor(false);
+//     }else if(props.id === 'cp'){
+//       props.id==='?setColor(true):setColor(false);
+//     }else if(props.id === 'ss'){
+//       props.id?setColor(true):setColor(false);
+//   }
+// }
+
+//   console.log('color', color)
+
   
 return(
     <div className="button">
