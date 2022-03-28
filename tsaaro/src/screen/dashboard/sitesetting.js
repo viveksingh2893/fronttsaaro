57944ced42c098254fcca952c25e04cf142ff688
Vivek <viveksingh2893@gmail.com>
2022-03-28 17:04:20 +0530

    Site Settings... one component left

--- tsaaro/src/screen/dashboard/sitesetting.js
+++ tsaaro/src/screen/dashboard/sitesetting.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import '../../assets/css/sitesetting.css';
-import { Checkbox, Radio, Typography, Divider } from 'antd';
+import {Typography, Switch} from 'antd';
 import {ReloadOutlined} from '@ant-design/icons';
 
 const { Paragraph } = Typography;
@@ -9,6 +9,20 @@ const { Paragraph } = Typography;
 const SiteSettingScr=()=>{
     const [url, setUrl] = useState('http://www.destratum.com');
     const [siteName, setSiteName] = useState('Destratum');
+    const [active, setActive]= useState('Active')
+    const [action, setAction] = useState(false)
+
+    useEffect(()=>{
+    if(action){
+        setActive('Inactive');
+    }else{
+        setActive('Active');
+    }
+    },[action])
+
+    const toggle=()=>{
+        setAction(!action);
+    }
     
     return (
       <div className="sitesetting-maincontainer">
@@ -26,26 +40,67 @@ const SiteSettingScr=()=>{
         <p className="sitesetting-description">Edit and manage the details of your website.</p>
         </div>
         <div className="sitesetting-container">
-            <div style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'flex-start'}}>
+            <div style={{display:'flex', flexDirection:'row',marginTop:'1vh', alignItems:'center', justifyContent:'flex-start'}}>
                 <p className="sitesetting-title">URL :</p>
                 <Paragraph editable={{ onChange: setUrl}} autoSize={true} style={{width:'30vw'}}>
                     {url}
                 </Paragraph>
             </div>
-            <div style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'flex-start'}}>
+            <div style={{display:'flex', flexDirection:'row',marginTop:'1vh', alignItems:'center', justifyContent:'flex-start'}}>
                 <p className="sitesetting-title">Site name :</p>
                 <Paragraph editable={{ onChange: setSiteName}} autoSize={true} style={{width:'30vw'}}>
                     {siteName}
                 </Paragraph>
             </div>
-            <div style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'flex-start', marginTop:'1vh'}}>
+            <div style={{display:'flex', flexDirection:'row',marginTop:'2vh', alignItems:'center', justifyContent:'flex-start'}}>
                 <p className="sitesetting-title">Plan :</p>
                 <p className="sitesetting-content">Ultimate</p>
             </div>
-            <div style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'flex-start', marginTop:'3vh'}}>
+            <div style={{display:'flex', flexDirection:'row',marginTop:'4vh', alignItems:'center', justifyContent:'flex-start'}}>
                 <p className="sitesetting-title">Banner Status :</p>
-                <p className="sitesetting-content" style={{width:'4vw'}}>Active</p>
-                <ReloadOutlined style={{}}/>
+                <p className="sitesetting-content" style={{width:'4vw'}}>{active}</p>
+                {action?
+                <ReloadOutlined spin onClick={toggle} style={{ fontSize: '1.3vw', fontWeight:'bold', color:'black'}}/>:
+                <ReloadOutlined onClick={toggle} style={{ fontSize: '1.3vw', fontWeight:'bold',color:'black' }}/>                
+                }
+            </div>
+            <div style={{display:'flex', flexDirection:'row',marginTop:'3vh', alignItems:'center', justifyContent:'flex-start'}}>
+                <p className="sitesetting-title">Schedule cookie scan :</p>
+                <form>
+                  <select id="scan" name="period" className="schedule-scan">
+                    <option value="never">Never</option>
+                    <option value="once" >Only Once</option>
+                    <option value="monthly" >Monthly</option>
+                  </select>
+                </form>
+            </div>
+            <div style={{display:'flex', flexDirection:'row',marginTop:'3vh', alignItems:'center', justifyContent:'flex-start'}}>
+                <p className="sitesetting-title">Enable consent log :</p>
+                <Switch size="small"/>
+            </div>
+            <div style={{display:'flex', flexDirection:'row',marginTop:'4vh', alignItems:'center', justifyContent:'flex-start'}}>
+                <p className="sitesetting-title">Required Banner languages :</p>
+                {/* <form>
+                  <select id="lang" name="language" className="schedule-scan">
+                    <option value="english">English</option>
+                    <option value="hindi" >Hindi</option>
+                    <option value="bulgarian" >Bulgarian</option>
+                    <option value="dutch" >Dutch</option>
+                    <option value="french" >French</option>
+                  </select>
+                </form> */}
+            </div>
+            <div style={{display:'flex', flexDirection:'row',marginTop:'4vh', alignItems:'center', justifyContent:'flex-start'}}>
+                <p className="sitesetting-title">Default Banner language : :</p>
+                <form>
+                  <select id="lang" name="language" className="schedule-scan">
+                    <option value="english">English</option>
+                    <option value="hindi" >Hindi</option>
+                    <option value="bulgarian" >Bulgarian</option>
+                    <option value="dutch" >Dutch</option>
+                    <option value="french" >French</option>
+                  </select>
+                </form>
             </div>
         </div>
       </div>
