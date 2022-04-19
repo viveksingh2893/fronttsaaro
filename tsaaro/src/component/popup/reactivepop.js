faa21ea3b67b2ead928e912a050397f9ccda7f66
Vivek <viveksingh2893@gmail.com>
2022-04-19 22:04:00 +0530

    Enter website popup

--- tsaaro/src/component/popup/reactivepop.js
+++ tsaaro/src/component/popup/reactivepop.js
@@ -6,7 +6,7 @@ const ReactivePop=(props)=> {
     <div className="as-web-modal">
         <div className="as-web-modal-content">
             <div className="as-web-reactivate">
-                <p className="as-web-pop-head">Do you want to Reactivate?</p>
+                <p className="as-web-pop-head">Do you want to reactivate the current plan?</p>
                 <div className="as-web-submitpop">
                     <button onClick={() => {props.closeReactive(false)}} className="as-web-cancel">Cancel</button>
                     <button onClick={() => {props.closeReactive(false)}} className="as-web-submit">Reactivate</button>
