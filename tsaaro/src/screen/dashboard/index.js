5a99b3d81bd63eb4383d8fa5101a5ff34c76ecdc
RaCoDev <itsmerana7@gmail.com>
2022-04-21 11:03:47 +0530

    redux store added

--- tsaaro/src/screen/dashboard/index.js
+++ tsaaro/src/screen/dashboard/index.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import HeaderMenuView from "../../component/header/HeaderMenuView";
 import SideMenuView from "../../component/header/SideMenuView";
@@ -12,13 +11,36 @@ import DashboardScr from "./dashboard";
 import SitesettingScr from "./sitesetting";
 import "../../assets/css/sidemenu.css";
 import AccsettingScr from "./accsetting";
+import Postapi from "../../api/Postapi";
+import Getapi from "../../api/Getapi";
+import { useDispatch } from "react-redux";
+
+import { changeWebsite } from "../../redux/action";
 
 const Screen = () => {
   const [inactive, setInactive] = useState(false);
   const [cbvisible, setCbvisible] = useState(false);
+  const [webList, setwebList] = useState([]);
+  const [selectItem, setItem] = useState();
 
-  console.log('inactive', inactive);
-  
+  const dispatch=useDispatch();
+  useEffect(() => {
+    CheckWeblist();
+  }, []);
+
+  const CheckWeblist = async () => {
+    const response = await Getapi("/auth/get_websitecheck");
+    if (response.data) {
+      if (response.data.website_check) {
+        const response = await Getapi("/auth/get_allwebsite");
+
+        setwebList(response.data);
+      }
+    }
+  };
+
+  console.log("inactive", inactive);
+
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
       {cbvisible ? (
@@ -37,19 +59,31 @@ const Screen = () => {
         />
       </div>
       <div style={{ display: "flex", flexDirection: "column" }}>
-        <HeaderMenuView inactive={inactive} setInactive={setInactive} />
-          <div style={{ display:"flex", width:inactive ? "93vw":"86vw", marginTop:'8vh', backgroundColor:"#E5E5E5"}}>
-            <Routes>
-              <Route path="/dash" element={<DashboardScr />} />
-              <Route path="/cm" element={<CookiemanagerScr />} />
-              <Route path="/cb" element={<ConsentbannerScr />} />
-              <Route path="/cl" element={<ConsentlogScr />} />
-              <Route path="/cp" element={<CookiepolicyScr />} />
-              <Route path="/pp" element={<PrivacypolicyScr />} />
-              <Route path="/ss" element={<SitesettingScr />} />
-              <Route path="/as" element={<AccsettingScr />} />
-            </Routes>
-          </div> 
+        <HeaderMenuView
+          inactive={inactive}
+          webList={webList}
+          setInactive={setInactive}
+          setItem={setItem}
+        />
+        <div
+          style={{
+            display: "flex",
+            width: inactive ? "93vw" : "86vw",
+            marginTop: "8vh",
+            backgroundColor: "#E5E5E5",
+          }}
+        >
+          <Routes>
+            <Route path="/dash" element={<DashboardScr />} />
+            <Route path="/cm" element={<CookiemanagerScr />} />
+            <Route path="/cb" element={<ConsentbannerScr />} />
+            <Route path="/cl" element={<ConsentlogScr />} />
+            <Route path="/cp" element={<CookiepolicyScr />} />
+            <Route path="/pp" element={<PrivacypolicyScr />} />
+            <Route path="/ss" element={<SitesettingScr />} />
+            <Route path="/as" element={<AccsettingScr />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
