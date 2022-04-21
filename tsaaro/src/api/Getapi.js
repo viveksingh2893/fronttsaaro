cb6c78b3570dbaf0f6b2d147e266905ce71eeeaa
RaCoDev <itsmerana7@gmail.com>
2022-04-21 11:05:14 +0530

    get api add

---
+++ tsaaro/src/api/Getapi.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+// const IP_ADDR=process.env.REACT_APP_IP_ADDR;
+const IP_ADDR='http://192.168.29.6:8001';
+console.log('address.......',IP_ADDR)
+
+
+const Getapi=async (url)=>{
+    console.log('address.....',IP_ADDR)
+    const res=await axios.get(`${IP_ADDR}${url}`)
+    .then(res=>{
+        if(res){
+            
+            return res
+
+        }
+
+    }
+
+    ).catch(({response}) => response)
+    
+    return res
+
+
+}
+export default Getapi;
